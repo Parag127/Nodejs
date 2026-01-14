560e66a843ec06684dd8600cffa0585c1b43e5a2
Parag Badgujar <badgujarparag419@gmail.com>
2026-01-14 19:35:37 +0530

    Nginx Server

---
+++ Handeling Files in Nodejs as Project/Project 2 with nodejs/server.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const fs = require('fs');
+const path = require('path')
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, req.url === '/' ? "index.html" : req.url);
+
+  const extName = path.extname(filePath).toLowerCase();
+
+  const mimeTypes = {
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "text/javaScript",
+    ".png" : "text/png",
+  }
+
+  const contentType = mimeTypes[extName] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err){
+      if (err.code === "ENOENT"){
+        res.writeHead("404", {"Content-Type" : "text/html"});
+        res.end(content, "File not found BRO");
+      }
+    } else {
+      res.writeHead(200, {"Content-Type": contentType});
+      res.end(content, "utf-8");
+    }
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+})
